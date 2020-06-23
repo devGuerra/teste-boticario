1c5042325f2d04ae528be3074adf240acc55a346
Roberto Guerra <roberto.santa@semparar.com.br>
2020-06-23 15:58:20 -0300

    start src folder and eslint config

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Init</Text>
+    </View>
+  );
+};
+
+export default App;
